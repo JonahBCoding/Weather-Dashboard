24ab9f88567bc4c73871306da2058dafcce01097
unknown <jonah.benson@gmail.com>
2020-11-01 16:07:14 -0700

    fiinished 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,28 @@
+const apiKey = "6db35a7c1d7e41c02d39de4643d89022"
+
 var search = document.querySelector("#srch-btn");
+var cityName = document.querySelector("#city-srch-input");
+
+// Search History Section
 
 
 //Load API Data
+// Current Weather Scetion
 var getWeather = function (e) {
     var cityName = document.querySelector("#city-srch-input").value;
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=6db35a7c1d7e41c02d39de4643d89022")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=" + apiKey)
         .then(r => r.json())
         .then(function (data) {
 
             console.log(data)
+
             //declared variables
             var temp = document.querySelector("#temperature")
             var humidity = document.querySelector("#humidity")
             var windSpeed = document.querySelector("#wind-speed")
             var cityName = document.querySelector("#city-name")
             var crntImg = document.querySelector("#current-img")
+
             // Current Weather Section
             $("#city-name").text(data.city.name);
             $("#current-img").attr("src", "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png");
@@ -49,10 +57,7 @@ var getWeather = function (e) {
                         $(".badge-warning").hide();
                     }
                 })
-
             console.log(coordLat, coordLon)
-
-
             console.log(document.querySelector("#temperature"))
             console.log(document.querySelector("#humidity"))
             console.log(document.querySelector("#wind-speed"))
@@ -60,6 +65,43 @@ var getWeather = function (e) {
             console.log(document.querySelector("#current-img"))
         })
 
+var day5Api = ("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=" + apiKey) 
+
+fetch (day5Api)
+
+        .then( r => r.json())
+        .then(function (data) {
+        console.log(data)
+        
+        $("#date-1").text(data.list[4].dt_txt)
+        $("#icon-1").attr("src", "http://openweathermap.org/img/w/" + data.list[4].weather[0].icon + ".png");
+        $("#temp-1").text("Temp: " + data.list[4].main.temp + "\u00b0F")
+        $("#hum-1").text("Humidity: " + data.list[4].main.humidity + " %");
+
+        $("#date-2").text(data.list[12].dt_txt)
+        $("#icon-2").attr("src", "http://openweathermap.org/img/w/" + data.list[12].weather[0].icon + ".png");
+        $("#temp-2").text("Temp: " + data.list[12].main.temp + "\u00b0F")
+        $("#hum-2").text("Humidity: " + data.list[12].main.humidity + " %") ;
+
+        $("#date-3").text(data.list[20].dt_txt)
+        $("#icon-3").attr("src", "http://openweathermap.org/img/w/" + data.list[20].weather[0].icon + ".png");
+        $("#temp-3").text("Temp: " + data.list[20].main.temp + "\u00b0F")
+        $("#hum-3").text("Humidity: " + data.list[20].main.humidity + " %") ;
+
+        $("#date-4").text(data.list[28].dt_txt)
+        $("#icon-4").attr("src", "http://openweathermap.org/img/w/" + data.list[28].weather[0].icon + ".png");
+        $("#temp-4").text("Temp: " + data.list[28].main.temp + "\u00b0F")
+        $("#hum-4").text("Humidity: " + data.list[28].main.humidity + " %");
+
+        $("#date-5").text(data.list[36].dt_txt)
+        $("#icon-5").attr("src", "http://openweathermap.org/img/w/" + data.list[36].weather[0].icon + ".png");
+        $("#temp-5").text("Temp: " + data.list[36].main.temp + "\u00b0F")
+        $("#hum-5").text("Humidity: " + data.list[36].main.humidity + " %")   
+        
+        })
+          
 }
 search.onclick = getWeather;
+
+
 
