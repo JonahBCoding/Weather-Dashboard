51e5d6c28d6fb717d27e63bd566ddabbd6e3edf0
unknown <jonah.benson@gmail.com>
2020-11-01 20:47:26 -0700

    recent Searches now displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,33 @@ const apiKey = "6db35a7c1d7e41c02d39de4643d89022"
 
 var search = document.querySelector("#srch-btn");
 var cityName = document.querySelector("#city-srch-input");
+var searchedCities = [];
 
-// Search History Section
+$(document).ready(function () {
+    getSearchHistory();
+})
 
+function getSearchHistory() {
 
-//Load API Data
+    $(".list-group").empty();
+    searchedCitiesString = localStorage.getItem("searchedCities");
+
+    searchedCities = JSON.parse(searchedCitiesString);
+    if (searchedCities === null) {
+        searchedCities = [];
+    }
+    console.log(searchedCities);
+
+    for (var i = searchedCities.length - 1; i >= 0; i--) {
+        var list = document.createElement('li');
+        list.setAttribute('class', "list-group-item list-group-item-action");
+        list.setAttribute('Onclick', 'research(this)');
+        list.setAttribute('value', searchedCities[i]);
+        list.innerText = searchedCities[i];
+        recentSearches.appendChild(list);
+    }
+};
+
 // Current Weather Scetion
 var getWeather = function (e) {
     var cityName = document.querySelector("#city-srch-input").value;
@@ -14,7 +36,7 @@ var getWeather = function (e) {
         .then(r => r.json())
         .then(function (data) {
 
-            console.log(data)
+
 
             //declared variables
             var temp = document.querySelector("#temperature")
@@ -57,51 +79,72 @@ var getWeather = function (e) {
                         $(".badge-warning").hide();
                     }
                 })
-            console.log(coordLat, coordLon)
-            console.log(document.querySelector("#temperature"))
-            console.log(document.querySelector("#humidity"))
-            console.log(document.querySelector("#wind-speed"))
-            console.log(document.querySelector("#city-name"))
-            console.log(document.querySelector("#current-img"))
+
         })
 
-var day5Api = ("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=" + apiKey) 
+    var day5Api = ("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=" + apiKey)
 
-fetch (day5Api)
+    fetch(day5Api)
 
-        .then( r => r.json())
+        .then(r => r.json())
         .then(function (data) {
-        console.log(data)
-        
-        $("#date-1").text(data.list[4].dt_txt)
-        $("#icon-1").attr("src", "http://openweathermap.org/img/w/" + data.list[4].weather[0].icon + ".png");
-        $("#temp-1").text("Temp: " + data.list[4].main.temp + "\u00b0F")
-        $("#hum-1").text("Humidity: " + data.list[4].main.humidity + " %");
 
-        $("#date-2").text(data.list[12].dt_txt)
-        $("#icon-2").attr("src", "http://openweathermap.org/img/w/" + data.list[12].weather[0].icon + ".png");
-        $("#temp-2").text("Temp: " + data.list[12].main.temp + "\u00b0F")
-        $("#hum-2").text("Humidity: " + data.list[12].main.humidity + " %") ;
 
-        $("#date-3").text(data.list[20].dt_txt)
-        $("#icon-3").attr("src", "http://openweathermap.org/img/w/" + data.list[20].weather[0].icon + ".png");
-        $("#temp-3").text("Temp: " + data.list[20].main.temp + "\u00b0F")
-        $("#hum-3").text("Humidity: " + data.list[20].main.humidity + " %") ;
+            $("#date-1").text(data.list[4].dt_txt)
+            $("#icon-1").attr("src", "http://openweathermap.org/img/w/" + data.list[4].weather[0].icon + ".png");
+            $("#temp-1").text("Temp: " + data.list[4].main.temp + "\u00b0F")
+            $("#hum-1").text("Humidity: " + data.list[4].main.humidity + " %");
 
-        $("#date-4").text(data.list[28].dt_txt)
-        $("#icon-4").attr("src", "http://openweathermap.org/img/w/" + data.list[28].weather[0].icon + ".png");
-        $("#temp-4").text("Temp: " + data.list[28].main.temp + "\u00b0F")
-        $("#hum-4").text("Humidity: " + data.list[28].main.humidity + " %");
+            $("#date-2").text(data.list[12].dt_txt)
+            $("#icon-2").attr("src", "http://openweathermap.org/img/w/" + data.list[12].weather[0].icon + ".png");
+            $("#temp-2").text("Temp: " + data.list[12].main.temp + "\u00b0F")
+            $("#hum-2").text("Humidity: " + data.list[12].main.humidity + " %");
 
-        $("#date-5").text(data.list[36].dt_txt)
-        $("#icon-5").attr("src", "http://openweathermap.org/img/w/" + data.list[36].weather[0].icon + ".png");
-        $("#temp-5").text("Temp: " + data.list[36].main.temp + "\u00b0F")
-        $("#hum-5").text("Humidity: " + data.list[36].main.humidity + " %")   
-        
+            $("#date-3").text(data.list[20].dt_txt)
+            $("#icon-3").attr("src", "http://openweathermap.org/img/w/" + data.list[20].weather[0].icon + ".png");
+            $("#temp-3").text("Temp: " + data.list[20].main.temp + "\u00b0F")
+            $("#hum-3").text("Humidity: " + data.list[20].main.humidity + " %");
+
+            $("#date-4").text(data.list[28].dt_txt)
+            $("#icon-4").attr("src", "http://openweathermap.org/img/w/" + data.list[28].weather[0].icon + ".png");
+            $("#temp-4").text("Temp: " + data.list[28].main.temp + "\u00b0F")
+            $("#hum-4").text("Humidity: " + data.list[28].main.humidity + " %");
+
+            $("#date-5").text(data.list[36].dt_txt)
+            $("#icon-5").attr("src", "http://openweathermap.org/img/w/" + data.list[36].weather[0].icon + ".png");
+            $("#temp-5").text("Temp: " + data.list[36].main.temp + "\u00b0F")
+            $("#hum-5").text("Humidity: " + data.list[36].main.humidity + " %")
+
         })
-          
+
 }
-search.onclick = getWeather;
+
+//search.onclick = getWeather;
+
+$("#srch-btn").on("click", function (event) {
+    event.preventDefault();
+
+    if ($("#city-srch-input").val() != "") {
+
+        city = $("#city-srch-input").val();
+
+
+        searchedCities.push(city);
+        localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
+
+        getSearchHistory();
+
+        getWeather();
+    }
+
+    $("#city-srch-input").val("");
+
+});
+
+
+
+
+
 
 
 
